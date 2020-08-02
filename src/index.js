5c9b15cbe3206a7ec966a4fd9e64685b036f0911
Behzad Pouyanfar <bzad0000@gmail.com>
2020-08-01 23:23:02 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AvailableProjects from "./AvailableProjects"
 
-ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById('root'));
+ReactDOM.render(<div className="container">
+<Navbar />
+<AvailableProjects />
+<Panel />
+</div>, document.getElementById('root'));
+
